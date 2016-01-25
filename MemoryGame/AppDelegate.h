2386a659273f8109df747f6f31f761f1c61c3c31
Wong You Jing <wongyoujing118@gmail.com>
2016-01-25 14:31:23 -0800

    Initial Commit

---
+++ MemoryGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryGame
+//
+//  Created by Wong You Jing on 25/01/2016.
+//  Copyright © 2016 NoNonsense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MemoryGame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MemoryGame
+//
+//  Created by Wong You Jing on 25/01/2016.
+//  Copyright © 2016 NoNonsense. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
